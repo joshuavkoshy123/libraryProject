58391d45b51b066fd7b403987513776e7bcf0b3e
Joshua Koshy <joshuavkoshy@gmail.com>
2025-11-25 14:26:24 -0600

    Search function

--- library_project/backend.py
+++ library_project/backend.py
@@ -14,11 +14,37 @@ def search(search_str):
                       FROM BOOK
                       JOIN BOOK_AUTHORS ON BOOK.isbn=BOOK_AUTHORS.isbn
                       JOIN AUTHORS ON BOOK_AUTHORS.author_id=AUTHORS.author_id
-                      WHERE LOWER(BOOK.isbn) LIKE %s OR LOWER(title) LIKE %s OR LOWER(AUTHORS.first_name) LIKE %s OR LOWER(AUTHORS.middle_name) LIKE %s OR LOWER(AUTHORS.last_name) LIKE %s;""",
+                      WHERE LOWER(BOOK.isbn) LIKE %s OR LOWER(title) LIKE %s OR LOWER(AUTHORS.first_name) LIKE %s OR LOWER(AUTHORS.middle_name) LIKE %s OR LOWER(AUTHORS.last_name) LIKE %s
+                      ORDER BY BOOK.isbn ASC;""",
                         (search_str, search_str, search_str, search_str, search_str))
     rows = cursor.fetchall()
 
+    seen = set()
+    duplicates = set()
+
     for row in rows:
-        print(row)
+        isbn = row[0]
+        if isbn in seen:
+            duplicates.add(isbn)
+        seen.add(isbn)
+
+    print("Duplicate ISBNs:", duplicates)
+
+    isbn = rows[0][0]
+    authors = ""
+    i = 0
+    while i < len(rows):
+        isbn = rows[i][0]
+        title = rows[i][1]
+        while (i < len(rows) and rows[i][0] == isbn):
+            middle_name = rows[i][3]
+            if (pd.isna(middle_name)):
+                middle_name = ""
+            authors = authors + rows[i][2] + " " + middle_name + " " + rows[i][4] + ", "
+            i = i + 1
+
+        authors = authors[:-2]
+        print(f"{isbn}, {title}, {authors}")
+        authors = ""
 
 search("WILL")
